504ed175b60cef2aaa2cda6ec9be73ac2814c2b1
JuliaVal821 <julia.kolgatina@gmail.com>
2021-08-03 01:56:58 -0400

    add tests for users feature add helper for uses endpoint add helper for config endpoint add common helper add global authorization hook update mocha timeout

--- helpers/auth.helper.js
+++ helpers/auth.helper.js
@@ -1,14 +1,14 @@
 import supertest from 'supertest';
 
 class AuthHelper {
-    constructor(){
+    constructor() {
         this.response = null;
     }
-    async post(login, password){
+    async post(login, password) {
           await supertest(process.env.BASE_URL)
             .post('/auth')
-            .send({ login: login, password: password})
-            .then((res) => {this.response = res});
+            .send({ login: login, password: password })
+            .then((res) => { this.response = res });
           return this.response;
     }
 
